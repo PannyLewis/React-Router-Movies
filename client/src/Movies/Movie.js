fb711b28d7d94539dbb28b6cbfb6b8012c75b387
Panny Lewis <pannylewis@gmail.com>
2020-06-06 01:41:57 -0500

    MovieCard.js:  created MovieCard component to replace block of duplicated codes in MovieList and Movie.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
@@ -31,10 +32,12 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
+      <MovieCard />
+
+      {/* <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -49,7 +52,8 @@ const Movie = (props) => {
             {star}
           </div>
         ))}
-      </div>
+      </div> */}
+
       <div className="save-button">Save</div>
     </div>
   );
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 const MovieList = (props) => {
   return (
@@ -12,10 +13,12 @@ const MovieList = (props) => {
 };
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
     <Link to={`/movies/${movie.id}`}>
-      <div className="movie-card">
+      <MovieCard />
+
+      {/* <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -23,14 +26,13 @@ function MovieDetails({ movie }) {
         <div className="movie-metascore">
           Metascore: <strong>{metascore}</strong>
         </div>
-        <h3>Actors</h3>
+        <h3>Actors</h3> */}
 
-        {stars.map((star) => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      {stars.map((star) => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
     </Link>
   );
 }
